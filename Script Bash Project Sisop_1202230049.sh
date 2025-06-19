a6ce395f882b2094df2cda583fc881534cb053c7
Rendyputra15 <rendyputra385@gmail.com>
2025-06-19 12:57:37 +0700

    Script Bash Project Tugas Sisop_1202230049.sh

--- Script Bash Project Sisop_1202230049.sh
+++ Script Bash Project Sisop_1202230049.sh
@@ -2,38 +2,41 @@
 
 # Warna
 biru="\e[36m"
+Biruu="\e[1;36m"
 kuning="\e[33m"
+cyan="\e[1;34m"
 ungu="\e[35m"
 putih="\e[1;37m"
 hijau="\e[32m"
-merah="\e[1;31m"
+MerahBold="\e[1;31m"
+merah="\e[31m"
 reset="\e[0m"
 
 clear
-figlet "MENU SISTEM" | lolcat
+figlet "    MENU SISTEM    " | lolcat
 
 while true; do
     echo ""
-    echo -e "${kuning}1.${reset} Tampilkan Kehidupan Saat Ini"
-    echo -e "${kuning}2.${reset} Tampilkan Daftar Direktori"
-    echo -e "${kuning}3.${reset} Informasi Jaringan"
-    echo -e "${kuning}4.${reset} Tampilkan Detail OS"
-    echo -e "${kuning}5.${reset} Tampilkan Waktu Install Pertama OS"
-    echo -e "${kuning}6.${reset} Informasi User"
-    echo -e "${kuning}7.${reset} Keluar"
-    echo -ne "\n${kuning}Pilih opsi [1-7]:${reset} "
+    echo -e "${ungu}1. Tampilkan Kehidupan Saat Ini${reset}"
+    echo -e "${ungu}2. Tampilkan Daftar Direktori${reset}"
+    echo -e "${ungu}3. Informasi Jaringan${reset}"
+    echo -e "${ungu}4. Tampilkan Detail OS${reset}"
+    echo -e "${ungu}5. Tampilkan Waktu Install Pertama OS${reset}"
+    echo -e "${ungu}6. Informasi User${reset}"
+    echo -e "${ungu}7. Keluar${reset}"
+    echo -ne "\n${ungu}Pilih opsi [1-7]: "
     read pilihan
 
     case $pilihan in
         1)
             nama="${ungu}Mas Rendy${reset}"
-            echo -e "\n${biru}Kehidupan Saat Ini:${reset} \nSEDANG MENUNGGU DIA KEMBALI..."
-            echo -e "\n${biru}Selamat Datang $nama! ${biru}Tanggal dan Waktu Saat Ini:${reset}"
+            echo -e "\n${cyan}Kehidupan Saat Ini:${reset} \nSEDANG MENUNGGU DIA KEMBALI..."
+            echo -e "\n${cyan}Selamat Datang $nama! ${cyan}Tanggal dan Waktu Saat Ini:${reset}"
             date +"%a %b %d %r WIB %Y"
             ;;
             
         2)
-            echo -e "\n${biru}Daftar Direktori:${reset}"
+            echo -e "\n${cyan}Daftar Direktori:${reset}"
             ls --color=always -l
             ;;
             
@@ -46,52 +49,52 @@ while true; do
             connection_info=$(nmcli -t -f DEVICE,TYPE,STATE,CONNECTION dev status)
             lokasi=$(curl -s ipinfo.io | jq -r '.city + ", " + .region + ", " + .country')
 
-            echo -e "\n${biru}Informasi Jaringan:${reset}"
+            echo -e "\n${cyan}Informasi Jaringan:${reset}"
             echo -e "${hijau}Alamat IP Lokal${reset}  : $ip_address"
             echo -e "${hijau}Gateway         ${reset} : $gateway"
             echo -e "${hijau}Netmask         ${reset} : $netmask"
             echo -e "${hijau}DNS Server(s)   ${reset} : $dns_server"
 
-            echo -e "\n${biru}Status Koneksi ke Internet:${reset}"
+            echo -e "\n${cyan}Status Koneksi ke Internet:${reset}"
             echo -e "$koneksi"
 
-            echo -e "\n${biru}Status Koneksi LAN/WIFI:${reset}"
+            echo -e "\n${cyan}Status Koneksi LAN/WIFI:${reset}"
             printf "${biru}%-10s %-10s %-10s %-20s${reset}\n" "DEVICE" "TYPE" "STATE" "CONNECTION"
             echo "$connection_info" | while IFS=: read -r dev type state conn; do
                 printf "%-10s %-10s %-10s %-20s\n" "$dev" "$type" "$state" "$conn"
             done
 
-            echo -e "\n${biru}Lokasi IP:${reset}"
+            echo -e "\n${cyan}Lokasi IP:${reset}"
             echo -e "$lokasi"
             ;;
 
         4)
-            echo -e "\n${biru}Detail OS:${reset}"
+            echo -e "\n${cyan}Detail OS:${reset}"
             echo -e "${putih}Nama OS\t\t:${reset} $(lsb_release -d | cut -f2)"
             echo -e "${putih}Versi\t\t:${reset} $(lsb_release -r | cut -f2) ($(lsb_release -c | cut -f2))"
             echo -e "${putih}ID\t\t:${reset} $(lsb_release -i | cut -f2)"
             echo -e "${putih}Keterangan\t:${reset} $(lsb_release -d | cut -f2)"
 
-            echo -e "\n${biru}Informasi Kernel:${reset}"
+            echo -e "\n${cyan}Informasi Kernel:${reset}"
             uname -r
 
-            echo -e "\n${biru}Proses CPU Terakhir:${reset}"
+            echo -e "\n${cyan}Proses CPU Terakhir:${reset}"
             top -bn1 | grep "%Cpu"
 
-            echo -e "\n${biru}Penggunaan Memori:${reset}"
+            echo -e "\n${cyan}Penggunaan Memori:${reset}"
             free -h
 
-            echo -e "\n${biru}Penggunaan Disk:${reset}"
+            echo -e "\n${cyan}Penggunaan Disk:${reset}"
             df -h | grep -E '^Filesystem|^/dev/'
             ;;
             
         5)
-            echo -e "\n${biru}Waktu OS Pertama Kali Diinstall:${reset}"
+            echo -e "\n${cyan}Waktu OS Pertama Kali Diinstall:${reset}"
             sudo head -n 1 "/var/log/installer/cloud-init.log" 2>/dev/null
             ;;
             
         6)
-            echo -e "\n${biru}Informasi Pengguna Saat Ini:${reset}"
+            echo -e "\n${cyan}Informasi Pengguna Saat Ini:${reset}"
             echo -e "Username        : $(whoami)"
             echo -e "User ID         : $(id -u)"
             echo -e "Group ID        : $(id -g)"
@@ -101,19 +104,19 @@ while true; do
             ;;
             
         7)
-            echo -e "\n${ungu}+----------------+${reset}"
-            echo -e "${ungu}| ${merah}Makasih Yaa :)${reset} ${ungu}|${reset}"
-            echo -e "${ungu}+----------------+${reset}"
+            echo -e "\n${hijau}+----------------+${reset}"
+            echo -e "${hijau}| ${MerahBold}Makasih Yaa :)${reset} ${hijau}|${reset}"
+            echo -e "${hijau}+----------------+${reset}"
             exit 0
             ;;
 
         *)
-            echo -e "\n${biru}Dicoba lagi ya Sayang!${reset}"
+            echo -e "\n${Biruu}Dicoba lagi ya Sayang!${reset}"
             ;;
     esac
 
     echo -e "\n${hijau}Tekan Enter untuk kembali ke Masa Lalu...${reset}"
     read
     clear
-    figlet "MENU SISTEM" | lolcat
+    figlet "    MENU SISTEM    " | lolcat
 done
